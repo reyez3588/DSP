84ac4b0b81643005f0b35e444bcacb3ce31a13a6
reyez3588 <reyez3588@gmail.com>
2021-11-19 18:18:52 -0600

    comparativa de graficas

--- test.py
+++ test.py
@@ -12,25 +12,38 @@ xw = x*w
 #print(len(x))
 
 # A partir de este punto estamos en el dominio de la frecuencia
+fig,ax = plt.subplots(1, 2, sharex=True, sharey=True)
 N = 256
-X = np.fft.fft(xw, N)
+freqs = np.arange(0, N/2)*fs/N
+
+X = np.fft.fft(x, N)
 print(X)
-Xaux = []
 for i in range (len(X)):
     X[i] = ((X[i].real)**2 + (X[i].imag)**2)**(1/2)
 print(X)
-XdB = 10 * np.log(X) # convertir a dB
-# f = m*fs/N
-valMax = max(X)
-m = 0
-for i in range(int((len(X)+1) / 2)):
-    if valMax == X[i]:
-        m = i
-print("La frecuencia es: ", m*fs/N)
+X[X<np.finfo(float).eps] = np.finfo(float).eps
+XdB = 20 * np.log(X) # convertir a dB
+ax[0].plot(freqs,XdB[0:128])
 
+XW = np.fft.fft(xw, N)
+#print(X)
+for i in range (len(X)):
+    XW[i] = ((XW[i].real)**2 + (XW[i].imag)**2)**(1/2)
+#print(X)
+XWdB = 20 * np.log(XW) # convertir a dB
+ax[1].plot(freqs,XWdB[0:128])
 
-plt.plot(XdB)
 plt.show()
 
+'''
+Para obtener el valor de la frecuencia 
+'''
+# f = m*fs/N
+valMax = max(XWdB)
+m = 0
+for i in range(int((len(X)+1) / 2)):
+    if valMax == XWdB[i]:
+        m = i
+print("La frecuencia es: ", m*fs/N)
 
 
